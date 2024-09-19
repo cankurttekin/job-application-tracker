b4d8ef30ab4b2c40a979f43a7f87ae6b8bf1ae1c
can kurttekin <cankurttekin@gmail.com>
2024-09-19 17:38:09 +0300

    microservices created

---
+++ JobApplicationService/src/test/java/com/kurttekin/JobApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurttekin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
