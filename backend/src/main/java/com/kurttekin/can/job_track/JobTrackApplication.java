4aceadb24519491bfd626f232e4f92737506c469
can kurttekin <cankurttekin@gmail.com>
2024-10-05 18:03:51 +0300

    backend project structure

---
+++ backend/src/main/java/com/kurttekin/can/job_track/JobTrackApplication.java
@@ -0,0 +1,13 @@
+package com.kurttekin.can.job_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackApplication.class, args);
+	}
+
+}
