58c1dc6c9d5dfba2d6dc6f60da4a4b1e9d40df24
can kurttekin <cankurttekin@gmail.com>
2024-10-07 19:16:19 +0300

    user regitration controller

--- src/main/java/com/kurttekin/can/job_track/presentation/rest/UserController.java
+++ src/main/java/com/kurttekin/can/job_track/presentation/rest/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserRegistrationRequest userRequest) {
         try {
             userService.registerUser(userRequest);
