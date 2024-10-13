940650a287267a510c383e346c168f88809e3544
can kurttekin <cankurttekin@gmail.com>
2024-10-13 05:33:56 +0300

    log allowed origins env var

--- backend/src/main/java/com/kurttekin/can/job_track/infrastructure/security/config/CorsConfig.java
+++ backend/src/main/java/com/kurttekin/can/job_track/infrastructure/security/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.kurttekin.can.job_track.infrastructure.security.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,21 +10,27 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Slf4j
 @Configuration
 public class CorsConfig {
 
-    @Value("${ALLOWED_ORIGINS}")
+    // if no env var, default to localhost:3000 for react
+    @Value("${ALLOWED_ORIGINS:http://localhost:3000}")
     private String allowedOrigins;
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         var source = new UrlBasedCorsConfigurationSource();
-
         CorsConfiguration configuration = new CorsConfiguration();
+
+        // Log the allowed origins
+        log.info("Allowed Origins: {}", allowedOrigins);
+
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         //configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "${ALLOWED_ORIGINS}"));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "${ALLOWED_ORIGINS}"));
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(","))); // Split the allowed origins
         configuration.setAllowCredentials(true);
         source.registerCorsConfiguration("/**",configuration);
 
