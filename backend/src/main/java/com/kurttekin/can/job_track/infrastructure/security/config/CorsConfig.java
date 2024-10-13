3345c4864621e7fad392f36d455b7e50166b0661
can kurttekin <cankurttekin@gmail.com>
2024-10-13 04:58:10 +0300

    cors env variable for deployment

--- backend/src/main/java/com/kurttekin/can/job_track/infrastructure/security/config/CorsConfig.java
+++ backend/src/main/java/com/kurttekin/can/job_track/infrastructure/security/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.kurttekin.can.job_track.infrastructure.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +12,9 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         var source = new UrlBasedCorsConfigurationSource();
@@ -18,7 +22,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.addAllowedOrigin("http://localhost:3000");
+        //configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "${ALLOWED_ORIGINS}"));
         configuration.setAllowCredentials(true);
         source.registerCorsConfiguration("/**",configuration);
 
