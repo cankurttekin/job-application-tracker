70b0ab9a6acbbd2df67eecf3641c5b84fad3d117
can kurttekin <cankurttekin@gmail.com>
2024-11-12 01:22:50 +0300

    Revert "main layout padding and home hero screenshot adjustment on mobile"
    This reverts commit c5342e51e8d26e34ee2c0e60971ef6eed6262ac0.

--- frontend/src/components/layout/MainContent.js
+++ frontend/src/components/layout/MainContent.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 const MainContent = styled.div`
     flex: 1;
     padding: 20px;
-    margin-top: 20px;
-    //margin: 20px;
+    margin: 20px;
     //background-color: #ffffff;
     //border-radius: 10px;
     //box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
